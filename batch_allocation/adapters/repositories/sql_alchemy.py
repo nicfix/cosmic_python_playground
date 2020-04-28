16efea740cac3296b6b582b98017b24c60b1cd5f
Nicola Sacco <n.sacco.dev@gmail.com>
2020-04-28 23:05:27 +0200

    Cleaned up the batch and order line repos and unit of work, migrated to product aggregate, Yuppi!

--- batch_allocation/adapters/repositories/sql_alchemy.py
+++ batch_allocation/adapters/repositories/sql_alchemy.py
@@ -15,31 +15,7 @@ class SQLAlchemyRepository(object):
         self.session = session
 
 
-class OrderLineSQLAlchemyRepository(OrderLineAbstractRepository, SQLAlchemyRepository):
-    def get(self, order_reference: str) -> [OrderLine]:
-        return list(self.session.query(OrderLine).filter_by(order_ref=order_reference))
-
-    def add(self, order_line: OrderLine):
-        self.session.add(order_line)
-        self.session.commit()
-
-
-class BatchSQLAlchemyRepository(BatchAbstractRepository, SQLAlchemyRepository):
-    def update(self, batch: Batch):
-        self.add(batch)
-
-    def get_by_sku(self, sku: str) -> [Batch]:
-        return list(self.session.query(Batch).filter_by(sku=sku))
-
-    def get(self, reference: str) -> Batch:
-        return self.session.query(Batch).filter_by(ref=reference).one()
-
-    def add(self, batch: Batch):
-        self.session.add(batch)
-        self.session.commit()
-
-
-class ProductSQLAlchemyRepository(AbstractProductRepository, BatchSQLAlchemyRepository):
+class ProductSQLAlchemyRepository(AbstractProductRepository, SQLAlchemyRepository):
 
     def get(self, sku: str) -> Product:
         product = self.session.query(Product).get(sku)
