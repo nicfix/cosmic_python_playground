9a0546692c86e62c2d78ea46d23f432b7e99173a
Nicola Sacco <n.sacco.dev@gmail.com>
2020-04-28 23:11:24 +0200

    Cleaned up skipped tests

--- batch_allocation/tests/integration/test_orm.py
+++ batch_allocation/tests/integration/test_orm.py
@@ -44,63 +44,6 @@ class OrmMappingIntegrationTestCase(BaseSessionTestCase):
 
         session.execute("DELETE FROM order_lines")
 
-    @skip('No batch can be created without a product')
-    def test_get_batch(self):
-        session = OrmMappingIntegrationTestCase.get_session()
-        session.execute(
-            "INSERT INTO batches (ref, sku, _purchased_quantity, eta) VALUES "
-            '("batch-1", "RED-CHAIR", 12, "2020-10-10")'
-        )
-
-        session.execute(
-            "INSERT INTO order_lines (order_ref, sku, quantity, allocated_batch_ref) VALUES "
-            '("order1", "RED-CHAIR", 12, "batch-1"),'
-            '("order1", "RED-TABLE", 13, "batch-1"),'
-            '("order1", "BLUE-LIPSTICK", 14, "batch-1")'
-        )
-
-        batches = session.query(Batch).all()
-
-        expected = [
-            OrderLine("order1", "RED-CHAIR", 12),
-            OrderLine("order1", "RED-TABLE", 13),
-            OrderLine("order1", "BLUE-LIPSTICK", 14),
-        ]
-
-        for batch in batches:
-            self.assertEqual(batch._allocated_order_lines, expected)
-
-        session.execute("DELETE FROM order_lines")
-
-        session.execute("DELETE FROM batches")
-
-    @skip('No direct allocation to a batch anymore, check the product one')
-    def test_allocate_order_line(self):
-        session = OrmMappingIntegrationTestCase.get_session()
-        batch = Batch("batch-1", "RED-CHAIR", 20, date.today())
-        order_line = OrderLine("order1", "RED-CHAIR", 12)
-
-        batch.allocate(order_line)
-
-        session.add(batch)
-
-        batches = session.query(Batch).all()
-
-        for bt in batches:
-            self.assertEqual(bt._allocated_order_lines, [order_line])
-
-        rows = list(
-            session.execute(
-                "SELECT order_ref, sku, quantity FROM order_lines WHERE order_ref = 'order1'"
-            )
-        )
-
-        self.assertEqual(len(rows), 1)
-
-        session.execute("DELETE FROM order_lines")
-
-        session.execute("DELETE FROM batches")
-
     def test_get_product(self):
         session = OrmMappingIntegrationTestCase.get_session()
         sku = str(uuid.uuid4())
