adeeea0a7d1a9b2ddb63bf5b59d0bd0501093749
Nicola Sacco <n.sacco.dev@gmail.com>
2020-05-09 16:49:27 +0200

    Added failing test

--- batch_allocation/tests/unit/service_layer/test_change_batch_quantity.py
+++ batch_allocation/tests/unit/service_layer/test_change_batch_quantity.py
@@ -1,7 +1,7 @@
 import uuid
 from unittest import TestCase
 
-from batch_allocation.domain.events import BatchQuantityChanged
+from batch_allocation.domain import events
 from batch_allocation.domain.model import Product, Batch
 from batch_allocation.service_layer import messagebus
 from batch_allocation.tests.unit.fixtures import MockedRepository, MockedUnitOfWork
@@ -10,31 +10,38 @@ from batch_allocation.tests.unit.fixtures import MockedRepository, MockedUnitOfW
 class ChangeBatchQuantityTestCase(TestCase):
 
     def setUp(self) -> None:
-        batchref = str(uuid.uuid4())
+        batchref_to_be_changed = str(uuid.uuid4())
+        other_batchref = str(uuid.uuid4())
         sku = 'Red Shoes'
-        original_quantity = 10
+        original_quantity = 20
 
-        batch = Batch(
-            ref=batchref,
-            sku=sku,
-            purchased_quantity=original_quantity,
-        )
+        mocked_unit_of_work = MockedUnitOfWork()
 
-        product = Product(
-            sku=sku,
-            batches=(batch,)
-        )
+        events_history = [
+            events.BatchCreated(
+                ref=batchref_to_be_changed,
+                sku=sku,
+                qty=original_quantity
+            ),
+            events.BatchCreated(
+                ref=other_batchref,
+                sku=sku,
+                qty=original_quantity
+            ),
+        ]
 
-        mocked_repository = MockedRepository((product,))
-        mocked_unit_of_work = MockedUnitOfWork(mocked_repository)
+        for event in events_history:
+            messagebus.handle(event, uow=mocked_unit_of_work)
 
-        self.batchref = batchref
         self.uow = mocked_unit_of_work
+        self.batchref_to_be_changed = batchref_to_be_changed
+        self.other_batchref = other_batchref
         self.sku = sku
+        self.original_quantity = original_quantity
 
     def test_decrease_quantity_no_re_allocation(self):
         new_quantity = 5
-        event = BatchQuantityChanged(sku=self.sku, ref=self.batchref, qty=new_quantity)
+        event = events.BatchQuantityChanged(sku=self.sku, ref=self.batchref_to_be_changed, qty=new_quantity)
 
         results = messagebus.handle(event, self.uow)
 
@@ -44,3 +51,57 @@ class ChangeBatchQuantityTestCase(TestCase):
         product = self.uow.products.get(self.sku)
         self.assertEqual(product.batches[0].available_quantity, new_quantity)
 
+    def test_decrease_quantity_reallocation_needed(self):
+        allocated_quantity = 20
+
+        # We need first to allocate the orders
+        allocation_events = [
+            events.AllocationRequired(
+                order_ref=str(uuid.uuid4()),
+                sku=self.sku,
+                qty=int(allocated_quantity / 2)
+            ),
+            events.AllocationRequired(
+                order_ref=str(uuid.uuid4()),
+                sku=self.sku,
+                qty=int(allocated_quantity / 2)
+            )
+        ]
+
+        allocated_events_count = len(allocation_events)
+
+        for event in allocation_events:
+            messagebus.handle(event, self.uow)
+
+        changed_batch = self.uow.products.get(self.sku).get_batch(self.batchref_to_be_changed)
+
+        # The quantity that is allocated is correct
+        self.assertEqual(allocated_quantity, changed_batch.allocated_quantity)
+        # All the order lines are allocated to the same batch
+        self.assertEqual(allocated_events_count, len(changed_batch.allocated_order_lines))
+
+        # Now we can test our quantity change
+        new_quantity = 10
+
+        allocation_change_event = events.BatchQuantityChanged(
+            ref=self.batchref_to_be_changed,
+            sku=self.sku,
+            qty=new_quantity
+        )
+
+        messagebus.handle(allocation_change_event, self.uow)
+
+        changed_batch = self.uow.products.get(self.sku).get_batch(self.batchref_to_be_changed)
+
+        # The quantity that is allocated is correct
+        self.assertEqual(new_quantity, changed_batch.purchased_quantity)
+
+        self.assertEqual(1, len(changed_batch.allocated_order_lines))
+
+        self.assertEqual(new_quantity - int(allocated_quantity / 2), changed_batch.available_quantity)
+
+        second_batch = self.uow.products.get(self.sku).get_batch(self.other_batchref)
+
+        self.assertEqual(1, len(second_batch.allocated_order_lines))
+
+        self.assertEqual(int(allocated_quantity / 2), second_batch.allocated_quantity)
